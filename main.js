edf8b7b97257302098b0310486d67ea4b42ecac6
Rakib Islam <74107377+rakibislamdev@users.noreply.github.com>
2021-08-30 12:45:36 +0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-<select class="form-control">
-  <option selected="selected">orange</option>
-  <option>white</option>
-  <option>purple</option>
-</select>
-
-$(".js-example-tags").select2({
-  tags: true
-});
